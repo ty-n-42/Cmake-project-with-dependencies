ce2e87510f6bb4f6f2d849becccdf420986227a1
Ty Newton <ty-newton@users.noreply.github.com>
2022-10-25 17:38:57 +1100

    basic structure and dependency dowload/build working

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Cmake-project-with-dependencies
 Trying to get cmake to work with dependencies
+
+Based on information from here: https://eliasdaler.github.io/using-cmake/
---
+++ src/main.c
@@ -0,0 +1,16 @@
+//
+// Created by bluepill on 23/10/2022.
+//
+// references:
+// https://eliasdaler.github.io/using-cmake/
+// https://pabloariasal.github.io/2018/02/19/its-time-to-do-cmake-right/
+//
+//
+
+#include "raylib.h"
+#include "enet/enet.h"
+
+int main()
+{
+    return 0;
+}
