e68ba6726d5ef491c4fd8042314a2db34a765782
Ty Newton <ty-newton@users.noreply.github.com>
2022-10-25 17:51:34 +1100

    basic structure and dependency dowload/build working

--- src/main.c
+++ src/main.c
@@ -4,7 +4,7 @@
 // references:
 // https://eliasdaler.github.io/using-cmake/
 // https://pabloariasal.github.io/2018/02/19/its-time-to-do-cmake-right/
-//
+// https://stackoverflow.com/questions/41392243/include-header-files-from-static-library-with-cmake (for how to find the hash of a branch)
 //
 
 #include "raylib.h"
